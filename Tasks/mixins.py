ec7586af0a0b7397310a7b94fb17be179f811eb1
vladultimate <shadowultimatereturn@gmail.com>
2025-05-17 21:53:41 +0300

    created new mixin (UserIsOwnerMixin) and added file mixins.py

---
+++ Tasks/mixins.py
@@ -0,0 +1,9 @@
+
+from django.core.exceptions import PermissionDenied
+
+class UserIsOwnerMixin:
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != request.user:
+            raise PermissionDenied("u are not owner of this board")
+        return super().dispatch(request, *args, **kwargs)
